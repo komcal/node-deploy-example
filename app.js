947346f7e8a15f9ef91c439bbb8e31cd0dd1333f
komcal <kom.sujautra@gmail.com>
2017-05-05 16:54:07 +0700

    chane port

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 
 var app = express();
+var PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen('8080', function() {
+app.listen(PORT, function() {
 
 console.log('listening port 8080');
 });
